accee9171bbda71fbd8e26f8cfadae6f574d9a4f
NiklasEndler <41130940+NiklasEndler@users.noreply.github.com>
2021-01-11 12:31:37 -0500

    navbar fix

--- test6.js
+++ test6.js
@@ -19,9 +19,13 @@
 $(window).scroll(function() {
 	if ($(document).scrollTop() > 50) {
 		$('nav').addClass('shrink');
+		$('nav').removeClass('navbar-light');
+		$('nav').addClass('navbar-dark');
 	}
 	else {
 		$('nav').removeClass('shrink');
+		$('nav').removeClass('navbar-dark');
+		$('nav').addClass('navbar-light');
 	}
 });
 
